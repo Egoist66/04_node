2e89ed2fe4ec1ae489cc48e4a0af9e6c964b626e
Farid Mahmudov <razormad666@gmail.com>
2024-10-02 17:13:50 +0300

    update

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,34 @@
 import express from 'express';
 
-const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).send({
-        message: 'Hello World!'});
+const app = express();
+const port = 3003;
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    console.log(req.url);
+    res.type('html');
+
+    res.status(200).send('Home');
+    
+
+
 });
+
+app.get('/users', (req, res) => {
+    res.status(200).json([{name: 'John'}, {name: 'Jane'}, {url: req.url}]);
+})
+
+app.post('/users', (req, res) => {
+    res.status(200).json({body: req.body});
+})
 
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
+
+
+
 });
 
